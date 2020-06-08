536d81a4af10ac500630ae87e57dd6e0757e19b4
yash <yashg0028@gmail.com>
2020-06-08 10:49:25 +0530

    new

---
+++ Practice/src/gurr99/Alerts.java
@@ -0,0 +1,33 @@
+package gurr99;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\gkuma222\\Documents\\ppp\\Practice\\driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+
+		String Baseurl ="http://demo.guru99.com/test/delete_customer.php";
+		driver.get(Baseurl);
+		driver.findElement(By.name("cusid")).sendKeys("Yash");
+		driver.findElement(By.name("submit")).click();
+		Thread.sleep(1000);
+		Alert al = driver.switchTo().alert();
+		String text= al.getText();
+		System.out.println(text);
+		Thread.sleep(1000);
+
+		al.accept();
+		al.accept();
+		
+		
+		
+		
+	}
+
+}
---
+++ Practice/src/gurr99/WindowHan.java
@@ -0,0 +1,51 @@
+package gurr99;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHan {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\gkuma222\\Documents\\ppp\\Practice\\driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("http://demo.guru99.com/popup.php");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//*[@style='font-size:16px;']")).click();
+		
+		
+		Set<String> s1=driver.getWindowHandles();
+		Iterator<String> it = s1.iterator();
+		
+		String Mainwin = it.next();
+		System.out.println(Mainwin);
+		 
+		String Childwin = it.next();
+		System.out.println(Childwin);
+		
+		if (Mainwin!=Childwin)
+		{
+			driver.switchTo().window(Childwin);
+			driver.findElement(By.xpath("//*[@maxlength='50']")).sendKeys("Yash@gmail.com");
+			driver.findElement(By.xpath("//*[@name='btnLogin']")).click();
+			Thread.sleep(1000);
+			driver.close();
+			
+		}
+		driver.switchTo().window(Mainwin);
+		String MainwinTitle= driver.getTitle();
+		System.out.println("On Main Window :" +MainwinTitle);
+		driver.quit();
+		
+		
+	}
+
+}
