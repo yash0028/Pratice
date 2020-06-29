1be423dcb1606d045c2797a17d5f2feddaf85cca
yash yashautomation0028@gmail.com <yash yashautomation0028@gmail.com>
2020-06-29 08:46:29 +0530

    new

--- Practice/src/com/qa/base/TestBase.java
+++ Practice/src/com/qa/base/TestBase.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import javax.jws.soap.SOAPBinding;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
