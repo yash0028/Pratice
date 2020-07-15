b98fe3674bb3bed45dffd7147042184a5281da9d
yash yashautomation0028@gmail.com <yash yashautomation0028@gmail.com>
2020-07-15 13:05:02 +0530

    new

---
+++ Practice/src/gurr99/DynamicWebTable.java
@@ -0,0 +1,56 @@
+package gurr99;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicWebTable {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Installation\\chromedriver.exe");
+		WebDriver driver;
+		driver = new ChromeDriver();
+
+		driver.get("http://techcanvass.com/Examples/webtable.html");
+
+		WebElement webtable=driver.findElement(By.xpath("html/body"));
+		
+		List<WebElement> rows=webtable.findElements(By.tagName("tr"));
+
+		System.out.println("Number of Rows including headings:"+ rows.size());
+
+		//Finding and printing number of columns
+		List<WebElement> columns = rows.get(0).findElements(By.tagName("th"));
+
+		System.out.println("Number of columns:"+ columns.size());
+
+		//Finding & Printing the column headings
+
+		for(int rnum=1;rnum<=rows.size();rnum++)
+		{
+			for (int colnum=1;colnum<=columns.size();colnum++)
+			{
+				if (rnum==1)
+				{
+					System.out.println(driver.findElement(By.xpath(	"//*[@id='t01']/tbody/tr[" + rnum + "]/th[" + colnum + "]")).getText());
+				}
+				else
+				{
+					System.out.println(driver.findElement(By.xpath(	"//*[@id='t01']/tbody/tr[" + rnum + "]/td[" + colnum + "]")).getText());
+				}
+			}
+		}
+		
+		    driver.quit();
+		    Thread.sleep(6000);
+		    if (driver != null)
+		         driver.quit();
+	
+		
+	}
+
+}
---
+++ Practice/src/gurr99/SWebTable.java
@@ -0,0 +1,62 @@
+package gurr99;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SWebTable {
+	
+
+
+		public static void main(String[] args) throws InterruptedException {
+			// TODO Auto-generated method stub
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Installation\\chromedriver.exe");
+			WebDriver driver;
+			driver = new ChromeDriver();
+
+			driver.get("http://techcanvass.com/Examples/webtable.html");
+			
+			
+			  for(int i=1;i<=1;i++) 
+			  { 
+				  for(int j=1;j<=3;j++)
+				  {
+			  System.out.println(driver.findElement(By.xpath("//*[@id='t01']/tbody/tr["+ i  +"]/th["+ j +"]")).getText()); 
+			      }
+				  }
+			  
+			  for(int rowno=2;rowno<=4;rowno++) 
+			  { 
+				  for(int colno=1;colno<=3;colno++)
+				  {
+			  System.out.println(driver.findElement(By.xpath("//*[@id='t01']/tbody/tr[" +rowno + "]/td[" + colno + "]")).getText()); 
+			  
+				  } 
+				  } 
+			  driver.close();
+			 
+			/*
+			 * Map<String,String> m1 = new HashMap<String,String>();
+			 * 
+			 * for(int i=1;i<=4;i++) { for(int j=1;j<=3;j++) { String key =
+			 * driver.findElement(By.xpath("//*[@id='t01']/tbody/tr[1]/th["+ j
+			 * +"]")).getText(); String value =
+			 * driver.findElement(By.xpath("//*[@id='t01']/tbody/tr[i+1]/td[" + j +
+			 * "]")).getText(); m1.put(key,value);
+			 * 
+			 * } } Set<Map.Entry<String, String>> s = m1.entrySet(); for (Map.Entry<String,
+			 * String> p1 :s) { String key=p1.getKey(); String b=p1.getValue();
+			 * System.out.println(key+" Details:"); System.out.println(b);
+			 * 
+			 * }
+			 */
+
+	}
+
+	}
+
+
