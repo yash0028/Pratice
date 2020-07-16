b82cd475518ee08a0d5dd71c47772219769a2faf
yash yashautomation0028@gmail.com <yash yashautomation0028@gmail.com>
2020-07-16 16:12:14 +0530

    mm

---
+++ Practice/src/gurr99/Amazon.java
@@ -0,0 +1,22 @@
+package gurr99;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\Installation\\chromedriver.exe");
+		WebDriver driver;
+		driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/search?q=amazon.in&oq=ama&aqs=chrome.1.69i57j35i39j0l5j46.4627j0j7&sourceid=chrome&ie=UTF-8");
+	
+	
+	
+	}
+
+}
